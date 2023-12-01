563d61a69b0f3f30ef1c58379f1c58a506754e65
Lucas Patrick Ramos de Oliveira <lpro@discente.ifpe.edu.br>
2023-12-01 10:13:19 -0300

    Limpando os comentários de AlunoDAOList.java

--- Padrao-de-Projeto-main/Padrao-de-Projeto-main/ExercicioPadroesProjetos-main/ExercicioPadroesProjetos-main/AtividadeAnalise/Exemplo/src/br/edu/ifpe/apoo/dao/AlunoDAOList.java
+++ Padrao-de-Projeto-main/Padrao-de-Projeto-main/ExercicioPadroesProjetos-main/ExercicioPadroesProjetos-main/AtividadeAnalise/Exemplo/src/br/edu/ifpe/apoo/dao/AlunoDAOList.java
@@ -25,31 +25,24 @@ public class AlunoDAOList implements AlunoDAO{
 	}
 
 	public void atualizar(Aluno aluno) {
-		//for (Aluno alunoAtualizar : alunos) {
 			if (alunoEncontrado(aluno.getId())) {
 				alunos.set((int)aluno.getId(), aluno.clone());
-			}
-		//}
-		
+			}	
 	}
 
 	public boolean remover(long id) {
-		//for (Aluno alunoRemover : alunos) {
 			if (alunoEncontrado(id)) {
 				alunos.remove((int)id);
 				return true;
 			}
-		//}
 		throw new RuntimeException("Aluno não foi removido!\nCertifique-se de digitar o ID correto!");
 	}
 
 	public Aluno get(long id) {
-		//for (Aluno alunoConsulta : alunos) {
 			if (alunoEncontrado(id)) {
 				
 				return alunos.get((int) id).clone();  
 			}
-		//}
 		throw new RuntimeException("Aluno não encontrado!\nCertifique-se de digitar o ID correto!");
 	}
 	
